48094f01da5fe2047aab66c60a290f6a6621a93d
User-XYZ <nwosu4686@gmail.com>
2023-10-25 16:00:53 +0100

    testing script

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,13 @@
 var displayName = function (myName) { return "My name is " + myName; };
 // console.log(displayName(true));    // flagged by typescript but not by javascript
 console.log(displayName('Tochi'));
+// Arrays
+var numbers = [10, 20, 30, 40];
+var animals = ['cat', 'dog', 'parrot'];
+var mixedTypes = ['Luigi', true, 'black belt', 3];
+numbers.push(50);
+// numbers.push('60');  // not allowed
+animals[2] = 'bird';
+// animals[1] = 9;   // not allowed
+mixedTypes.push('Antonio');
+mixedTypes[1] = false;
--- sandbox.ts
+++ sandbox.ts
@@ -2,3 +2,19 @@ const displayName = (myName: string) => "My name is " + myName;
 
 // console.log(displayName(true));    // flagged by typescript but not by javascript
 console.log(displayName('Tochi'));
+
+// Arrays
+
+let numbers = [ 10, 20, 30, 40 ];
+let animals = [ 'cat', 'dog', 'parrot' ];
+let mixedTypes = [ 'Luigi', true, 'black belt', 3 ];
+
+numbers.push(50);
+// numbers.push('60');  // not allowed
+
+animals[2] = 'bird';
+// animals[1] = 9;   // not allowed
+
+mixedTypes.push('Antonio');
+mixedTypes[1] = false;
+
