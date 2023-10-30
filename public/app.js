21fa3a0bec06fb761964296bb338a8f4d8484dae
User-XYZ <nwosu4686@gmail.com>
2023-10-30 20:51:52 +0100

    working with form

--- public/app.js
+++ public/app.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('app.ts');
+let form = document.querySelector('.new-item-form');
+// get input fields
+let type = document.querySelector('#type');
+let toFrom = document.querySelector('#tofrom');
+let details = document.querySelector('#details');
+let amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let values = {
+        type: type.value,
+        toFrom: toFrom.value,
+        details: details.value,
+        amount: amount.valueAsNumber
+    };
+    console.log(values);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('app.ts');
+
+let form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// get input fields
+let type = document.querySelector('#type') as HTMLSelectElement;
+let toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+let details = document.querySelector('#details') as HTMLInputElement;
+let amount = document.querySelector('#amount') as HTMLInputElement;
+
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    let values = {
+        type: type.value,
+        toFrom: toFrom.value,
+        details: details.value,
+        amount: amount.valueAsNumber
+    };
+
+    console.log(values);
+});
