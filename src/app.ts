11dbb5d0dac2f3117cd9ed42825f06604f49cbf1
User-XYZ <nwosu4686@gmail.com>
2023-11-02 17:08:43 +0100

    made adjustments to code

--- src/app.ts
+++ src/app.ts
@@ -4,39 +4,6 @@ import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interface/HasFormatter.js";
 
 
-// interface IsPerson {
-//     name: string;
-//     age: number;
-//     isMarried: boolean;
-//     speak(): void;
-//     add(a: number, b: number): number; 
-// }
-
-// const me: IsPerson = {
-//     name: 'Trump',
-//     age: 70,
-//     isMarried: true,
-//     speak() {
-//         console.log('Hi');
-//     },
-//     add(num1, num2) {
-//         return num1 + num2;
-//     }
-// }
-
-// console.log(me);
-
-// let doc1: HasFormatter;
-// let doc2: HasFormatter;
-
-// doc1 = new Invoivce('Donald', 'work on website', 300);
-// doc2 = new Payment('Bill', 'work on plumbing', 100);
-
-// console.log(doc1);
-// console.log(doc2);
-
-
-
 // get form element
 let form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -68,6 +35,4 @@ form.addEventListener('submit', (e: Event) => {
     }
 
     list.render(doc, type.value, 'end');
-    // console.log(doc);
-
 });
